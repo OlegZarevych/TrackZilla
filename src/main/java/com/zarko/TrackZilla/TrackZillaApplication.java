0bb7b1f8aee8c8bcfac162014b2b07e1c79eb22e
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2020-04-25 15:20:03 +0300

    initial commit

---
+++ src/main/java/com/zarko/TrackZilla/TrackZillaApplication.java
@@ -0,0 +1,15 @@
+package com.zarko.TrackZilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackZillaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TrackZillaApplication.class, args);
+		System.out.println("Hello from JAVA SPRING FRAMEWORK");
+	}
+
+}
