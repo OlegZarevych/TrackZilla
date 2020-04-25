4f19a074eb9b31b6ab7ae449640d7b08d6a378d5
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2020-04-26 01:27:07 +0300

    Add GraphQL mutator

--- src/main/java/com/zarko/TrackZilla/mutator/Mutation.java
+++ src/main/java/com/zarko/TrackZilla/mutator/Mutation.java
@@ -1,4 +1,42 @@
 package com.zarko.TrackZilla.mutator;
 
-public class Mutation {
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.zarko.TrackZilla.entity.Application;
+import com.zarko.TrackZilla.exception.ApplicationNotFoundException;
+import com.zarko.TrackZilla.repository.ApplicationRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Mutation implements GraphQLMutationResolver {
+    private ApplicationRepository appRepository;
+
+    public Mutation (ApplicationRepository appRepository){
+        this.appRepository = appRepository;
+    }
+
+    public Application newApplication(String name, String description, String owner){
+        Application application = new Application(name, owner, description);
+        appRepository.save(application);
+        return application;
+    }
+
+    public boolean deleteApplication(long id){
+        appRepository.deleteById(id);
+        return true;
+    }
+
+    public Application updateApplicationOwner(String newOwner, long id){
+        Optional<Application> app = appRepository.findById(id);
+
+        if (app.isPresent()) {
+            Application application = app.get();
+            application.setOwner(newOwner);
+            appRepository.save(application);
+            return application;
+        }
+        else
+            throw new ApplicationNotFoundException("Application with Id:" + id + "not found");
+    }
 }
